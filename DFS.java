e817ac98cb5780bf82573db305716e562701236e
Creusel <victor.creusel@gmail.com>
2017-01-19 18:12:56 +0100

    Init

---
+++ DFS.java
@@ -0,0 +1,136 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class DFS
+{
+    
+    public static void botched_dfs1(Graph g, int s){
+	Stack<Integer> stack = new Stack<Integer>();
+	boolean visited[] = new boolean[g.vertices()];
+	stack.push(s);
+	visited[s] = true;	    	
+	while (!stack.empty()){
+	    int u = stack.pop();
+	    System.out.println(u);
+	    for (Edge e: g.next(u))
+		if (!visited[e.to])
+		    {
+			visited[e.to] = true;
+			stack.push(e.to);
+		    }
+	}
+    }
+
+    public static void botched_dfs2(Graph g, int s){
+	Stack<Integer> stack = new Stack<Integer>();
+	boolean visited[] = new boolean[g.vertices()];
+	stack.push(s);
+	System.out.println(s);
+	visited[s] = true;	    	
+	while (!stack.empty()){
+	    int u = stack.pop();
+	    for (Edge e: g.next(u))
+		if (!visited[e.to])
+		    {
+			System.out.println(e.to);			
+			visited[e.to] = true;
+			stack.push(e.to);
+		    }
+	}
+    }
+    
+    public static void botched_dfs3(Graph g, int s){
+	Stack<Integer> stack = new Stack<Integer>();
+	boolean visited[] = new boolean[g.vertices()];
+	stack.push(s);
+	while (!stack.empty()){
+	    int u = stack.pop();
+	    if (!visited[u]){
+		visited[u] = true;
+		System.out.println(u);
+		for (Edge e: g.next(u))
+		    if (!visited[e.to])
+			   stack.push(e.to);
+		
+	    }
+	}
+    }
+
+    
+    public static List<Integer> tritopo(Graph g, int s){
+	    ArrayList<Integer> res = new ArrayList<Integer>(g.vertices());
+	    	
+		Stack<Integer> stack = new Stack<Integer>();
+		boolean visited[] = new boolean[g.vertices()];
+		stack.push(s);
+		visited[s] = true;
+		System.out.println(s);
+		while (!stack.empty()){
+		    boolean end = true;
+		    /* (a) Soit u le sommet en haut de la pile */
+		    /* (b) Si u a un voisin non visité, alors */
+		    /*     (c) on le visite et on l'ajoute sur la pile */
+		    /* Sinon */
+		    /*     (d) on enlève u de la pile */
+		   
+		    /* (a) */
+		    int u = stack.peek();
+		    for (Edge e: g.next(u))
+			if (!visited[e.to]) /* (b) */
+			    {
+				visited[e.to] = true;
+				System.out.println(e.to);			
+				stack.push(e.to); /*(c) */
+				end = false;
+				break;
+			    }
+		    if (end) /*(d)*/{
+		    	res.add(stack.peek());
+				stack.pop();
+		    }
+		}
+		
+		
+		res.trimToSize();
+		for(int i=0; i < res.size()/2; i++){
+			int tmp = res.get(i);
+			res.set(i, res.get(res.size()-1-i));
+			res.set(res.size()-1-i, tmp);
+		}
+		return res;
+    }
+
+
+    
+
+
+    
+    
+    public static void testGraph()
+    {
+	int n = 5;
+	int i,j;
+	GraphArrayList g = new GraphArrayList(6);
+	g.addEdge(new Edge(0, 1, 1));
+	g.addEdge(new Edge(0, 2, 1));
+	g.addEdge(new Edge(0, 3, 1));
+	g.addEdge(new Edge(1, 4, 1));
+	g.addEdge(new Edge(4, 3, 1));
+	g.addEdge(new Edge(3, 5, 1));
+	g.addEdge(new Edge(5, 1, 1));
+	//botched_dfs1(g, 0);
+	//botched_dfs2(g, 0);
+	//botched_dfs3(g, 0);
+	//botched_dfs4(g, 0);
+
+	List<Integer> res = tritopo(g, g.vertices()-2);
+	System.out.println(res);
+
+    }
+    
+    public static void main(String[] args)
+    {
+	testGraph();
+    }
+}
---
+++ SeamCarving.java
@@ -0,0 +1,236 @@
+import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+public class SeamCarving
+{
+
+   public static int[][] readpgm(String fn)
+	 {		
+        try {
+            //InputStream f = ClassLoader.getSystemClassLoader().getResourceAsStream(fn);
+            BufferedReader d = new BufferedReader(new InputStreamReader(new FileInputStream(fn)));
+            String magic = d.readLine();
+            String line = d.readLine();
+		   while (line.startsWith("#")) {
+			  line = d.readLine();
+		   }
+		   Scanner s = new Scanner(line);
+		   int width = s.nextInt();
+		   int height = s.nextInt();		   
+		   line = d.readLine();
+		   s = new Scanner(line);
+		   int maxVal = s.nextInt();
+		   int[][] im = new int[height][width];
+		   s = new Scanner(d);
+		   int count = 0;
+		   while (count < height*width) {
+			  im[count / width][count % width] = s.nextInt();
+			  count++;
+		   }
+		   return im;
+        }
+		
+        catch(Throwable t) {
+            t.printStackTrace(System.err) ;
+            return null;
+        }
+    }
+   
+   public static void writepgm(int[][] image, String filename){
+	   try {
+		   
+		PrintWriter ow = new PrintWriter(new BufferedWriter(new FileWriter(filename)));
+		ow.println("P2"); //magic number for PGM
+		ow.print(image[0].length); //number of columns
+		ow.print(" ");
+		ow.println(image.length); //number of lines
+		ow.println(65535); //max intensity
+		
+		for(int i=0; i < image.length; i++){
+			for(int j=0; j < image[i].length; j++){
+				ow.print(image[i][j] + " ");
+			}
+			ow.println();
+		}
+		
+		ow.close();
+		
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+   }
+   
+   public static int[][] interest(int[][] image){
+	   int[][] res = new int[image.length][image[0].length];
+	   
+	   for(int i=0; i < image.length; i++){
+		   for(int j=0; j < image[i].length; j++){
+			   res[i][j] = fInterest(image, i, j);
+		   }
+	   }
+	   
+	   return res;
+   }
+   
+   private static int fInterest(int[][] image, int i, int j){
+	   if(j == 0){
+		   return Math.abs(image[i][j] - image[i][1]); //no left neighbor
+	   }
+	   if(j == image[0].length-1){
+		   return Math.abs(image[i][j] - image[i][image[0].length-2]); //no right neighbor
+	   }
+	   return Math.abs(image[i][j] - (image[i][j-1] + image[i][j+1])/2);
+   }
+   
+   
+   public static Graph tograph(int[][] itr){
+	   GraphArrayList g = new GraphArrayList(itr.length*itr[0].length + 2/*A and P*/);
+	   for(int i=0; i < itr.length-1; i++){
+		   for(int j=0; j < itr[i].length; j++){
+			   int src = i*itr.length + j;
+			   int targetBase = src + itr.length;
+			   g.addEdge(new Edge(src, targetBase, itr[i][j]));
+			   
+			   if(j != 0){
+				   g.addEdge(new Edge(src, targetBase-1, itr[i][j]));
+			   }
+			   if(j != itr[i].length-1){
+				   g.addEdge(new Edge(src, targetBase+1, itr[i][j]));
+			   }
+		   }
+	   }
+	   
+	   //A edges
+	   for(int i=0; i < itr[0].length; i++){
+		   g.addEdge(new Edge(g.vertices()-2, i, 0));
+	   }
+	   
+	   //P edges
+	   final int nbLines = itr.length;
+	   final int nbCols = itr[0].length;
+	   for(int i=0; i < itr[0].length; i++){
+		   g.addEdge(new Edge((nbLines-1)*nbCols + i, g.vertices()-1, itr[nbLines-1][i]));
+	   }
+	   
+	   return g;
+   }
+   
+   
+   public static List<Integer> Bellman(Graph g, ArrayList<Integer> order){
+	   List<Integer> res = new ArrayList<Integer>();
+	   int T[] = new int[g.vertices()];
+	   T[order.get(0)] = 0;
+	   order.remove(0);
+	   
+	   boolean finished = false;
+	   while(! finished){
+		   int vertex = order.get(0);
+		   System.out.println("vertex : " + vertex);
+		   order.remove(0);
+		   finished = order.isEmpty();
+		   
+		   int min = 70000; //infinite
+		   int vertexOfMin = vertex;
+		   for(Edge neighbor : g.prev(vertex)){
+			   int cost = neighbor.cost + T[neighbor.from];
+			   if(cost < min){
+				   min = cost;
+				   vertexOfMin = neighbor.from;
+			   }
+		   }
+		   T[vertex] = min < T[vertex] ? min : T[vertex];
+		   res.add(vertexOfMin);
+		   
+	   }
+	   
+	   return res;
+   }
+   
+   
+   public static void testWrite(){
+	   int[][] image = new int[2][2];
+	   image[0][1] = 1;
+	   image[1][1] = 6543;
+	   SeamCarving.writepgm(image, "test.pgm");
+   }
+   
+   public static void testRead(){
+	   int[][] image = readpgm("test.pgm");
+	   
+	   StringBuilder sb = new StringBuilder();
+	   for(int i=0; i < image.length; i++){
+		   for(int j=0; j < image[i].length; j++){
+			   sb.append(image[i][j] + " ");
+		   }
+		   sb.append("\n");
+	   }
+	   System.out.println("test de read:");
+	   System.out.println(sb.toString());
+   }
+   
+   public static void testInterest(){
+	   int[][] image = new int[2][3];
+	   image[0][0] = 1;
+	   image[0][1] = 5;
+	   image[0][2] = 1;
+	   image[1][0] = 2;
+	   image[1][1] = 2;
+	   image[1][2] = 9;
+	   int[][] res = SeamCarving.interest(image);
+	   
+	   System.out.println("test de interest:");
+	   for(int i=0; i < res.length; i++){
+		   for(int j=0; j < res[i].length; j++){
+			   System.out.print(res[i][j] + " ");
+		   }
+		   System.out.println();
+	   }
+	   
+   }
+   
+   public static void testGraph(){
+	   int[][] itr = new int[2][2];
+	   itr[1][1] = 45;
+	   itr[0][0] = 7;
+	   itr[0][1] = 6;
+	   Graph g = tograph(itr);
+	   g.writeFile("output.txt");
+   }
+   
+   public static void testbellman(){
+	   int[][] itr = new int[2][2];
+	   itr[0][0] = 0;
+	   itr[0][1] = 55;
+	   itr[1][0] = 0;
+	   itr[1][1] = 78;
+	   
+	   Graph g = tograph(itr);
+	   g.writeFile("output.txt");
+	   ArrayList<Integer> order = (ArrayList<Integer>) DFS.tritopo(g, g.vertices()-2);
+	   System.out.println(order);
+	   List<Integer> path = Bellman(g, order);
+	   
+	   System.out.println(path);
+   }
+   
+   
+   public static void main(String args[]){
+	   //SeamCarving.testWrite();
+	   //SeamCarving.testRead();
+	   //SeamCarving.testInterest();
+	   //SeamCarving.testGraph();
+	   SeamCarving.testbellman();
+	   /*ArrayList<Integer> b = new ArrayList<Integer>();
+	   b.add(1);
+	   b.add(2);
+	   b.add(0);
+	   System.out.println(b.get(0));
+	   b.remove(0);
+	   System.out.println(b);*/
+
+   }
+
+   
+}
