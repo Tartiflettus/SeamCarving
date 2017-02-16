d965174b31ceeb3c33c745d178e95fe3be6d6163
Creusel <victor.creusel@gmail.com>
2017-02-16 18:06:52 +0100

    Ecriture de GraphImplicit non testé

--- GraphImplicit.java
+++ GraphImplicit.java
@@ -1,26 +1,59 @@
+import java.awt.HeadlessException;
 import java.util.ArrayList;
 
 class GraphImplicit implements Graph{
-    int N;
+    private int N;
+    private int[][] interest;
+    private int w;
+    private int h;
+    
     
 @SuppressWarnings("unchecked")    
-    GraphImplicit(int N){
-	this.N = N;
+    GraphImplicit(int interest[][], int w, int h){
+		this.N = w*h + 2;
+		this.w = w;
+		this.h = h;
+		this.interest = interest;
     }
 
     public int vertices(){
-	return N;
+    	return N;
     }
     
-@SuppressWarnings("unchecked")    
+    @SuppressWarnings("unchecked")    
     public Iterable<Edge> next(int v)
-	 {
-	     ArrayList<Edge> edges = new ArrayList();
-	     for (int i = v; i < N; i++)
-		  edges.add(new Edge(v,i,i));
+	{
+	     ArrayList<Edge> edges = new ArrayList<Edge>();
+	     
+	     if(v == vertices()-1){
+	    	 return edges;
+	     }
+	     
+	     if(v == vertices()-2){
+	    	 for(int i=0; i < w; i++){
+	    		 edges.add(new Edge(v, i, 0));
+	    	 }
+	    	 return edges;
+	     }
+	     
+	     if(v >= w*h - w){
+	    	 edges.add(new Edge(v, vertices()-1, interest[v/w][v%w]));
+	    	 return edges;
+	     }
+	     
+	     edges.add(new Edge(v, v + w, interest[v/w][v%w]));
+	     if(v%w != 0){
+	    	 edges.add(new Edge(v, v+w-1, interest[v/w][v%w]));
+	     }
+	     if(v%w != w-1){
+	    	 edges.add(new Edge(v, v+w+1, interest[v/w][v%w]));
+	     }
 	     return edges;
 		      
-	 }
+	}
+
+
+
 @SuppressWarnings("unchecked")
    public Iterable<Edge> prev(int v)
 	 {
@@ -30,6 +63,4 @@ class GraphImplicit implements Graph{
 	     return edges;
 		      
 	 }
-
-    
 }
